0beb28d2fab1455f628beb5dcd28548a07ad7eea
natemartinez <nathanielmartinez2001@gmail.com>
2024-02-12 23:18:10 -0600

    adjusting cors headers

--- server/server.js
+++ server/server.js
@@ -8,12 +8,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header(
+  res.setHeader(
     "Access-Control-Allow-Origin",
     "https://branchtest.onrender.com"
   );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
